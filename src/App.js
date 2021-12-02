6ad98608400b1cfcbec680aba37ac3787af1b662
Pedro Barbosa <78980796+pedrobarbosaocb@users.noreply.github.com>
2021-12-01 23:17:52 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
+import './App.css'
+import React from 'react'
+import TodoList from './Components/TodoList'
 
+function App() {
+  return (
+    <div className="todo-app">
+      
+      <TodoList className="test" />
+
+      <a href="#" className="logo">
+        <text id="pomodLOGO">POMOD</text>
+        <text id="orcLOGO">ORC</text>
+      </a>
+
+    </div>
+  )
+}
+
+export default App
